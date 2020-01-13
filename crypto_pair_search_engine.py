47eeb899f0bbb54d38804d0654351c900f88c3ee
Mike Chan <mike@mikewchan.com>
2020-01-13 10:12:33 -0500

    Add files via upload

--- crypto_pair_search_engine.py
+++ crypto_pair_search_engine.py
@@ -4,34 +4,41 @@ import cryptowatch as cw
 # Think about whether they will enter the entire name or the 3-letter ticker
 # Will eventually need this to be a dropdown list with search
 
-currency1a_input = input('Enter the first currency of the trading pair: ')
+currencypair1_input1 = input('Enter the abbreviation for the 1st currency of the 1st trading pair to search: ')
+currencypair1_input2 = input('Enter the abbreviation for the 2nd currency of the 1st trading pair to search: ')
 
-# Input Validation - check to see if the input exists and is all alphabetical, then changes to lowercase to search
-# figure this out later
-
-# if len(currency1_input) > 0 and currency1_input.isalpha():
-#    currency1 = currency1_input.lower()
-# else:
-#    print("Please enter the currency correctly: ")
-
-currency1b_input = input('Enter the second currency of the trading pair: ')
-
-# Repeat Input Validation for currency2_input
+answer = None
+while answer not in ("y", "Y", "n", "N"):
+    answer = input("Would you like to search for another trading pair? Enter y/n: ")
+    if answer == "y":
+        currencypair2_input1 = input(
+            'Enter the abbreviation for the 1st currency of the 2nd trading pair to search: ')
+        currencypair2_input2 = input(
+            'Enter the abbreviation for the 2nd currency of the 2nd trading pair to search: ')
+    elif answer == "n":
+        break
+    else:
+        print("Please enter either 'y' or 'n' only.")
 
 all_markets = cw.markets.list()
 
-market_pair_1a = currency1a_input+currency1b_input
-market_pair_1b = currency1b_input+currency1a_input
+currencypair1_combo1 = currencypair1_input1+currencypair1_input2
+currencypair1_combo2 = currencypair1_input2+currencypair1_input1
+currencypair2_combo1 = currencypair2_input1+currencypair2_input2
+currencypair2_combo2 = currencypair2_input2+currencypair2_input1
+
+print(currencypair2_combo1, currencypair2_combo2)
+
 market_exchange_count = 0
 
 
 # Need to eventually turn this into a function to apply to other inputted market pairs
 for market in all_markets.markets:
-    if market.pair == market_pair_1a:
-        print("{} has a {} market".format(market.exchange, market_pair_1a))
+    if market.pair == currencypair1_combo1:
+        print("{} has a {} market".format(market.exchange, currencypair1_combo1))
         market_exchange_count = market_exchange_count + 1
-    elif market.pair == market_pair_1b:
-        print("{} has a {} market".format(market.exchange, market_pair_1b))
+    elif market.pair == currencypair1_combo2:
+        print("{} has a {} market".format(market.exchange, currencypair1_combo2))
         market_exchange_count = market_exchange_count + 1
 
 if market_exchange_count == 0:
